b6bc57c2031a6c4a18b2513f5cb8f758b7378341
IvanchikAleksandr <Ivanchikaleksandr11@gmail.com>
2023-03-11 18:49:02 +0300

    Registration test.

---
+++ src/test/java/SingUpTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SingUpTest {
+    @Test
+
+    public void validZipCode() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register has not been opened ,register button is not dispayed");
+
+    }
+}
